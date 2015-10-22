86a8a06fc9a67ba2c90bdeda54e976c71ed2bd0f
Johan Eckerström <johan@duh.se>
2015-10-22 13:42:02 +0200

    Ability to configure ttl
    Previously we used nil, but 0 is what Dalli uses to describe "no
    expire". Dalli runs `#to_i` on the ttl before using it.

--- spec/lib/url_cache_spec.rb
+++ spec/lib/url_cache_spec.rb
@@ -4,10 +4,25 @@ describe Twingly::UrlCache do
 
   subject(:cache) { described_class.new }
 
+  it { is_expected.to respond_to(:ttl) }
   it { is_expected.to respond_to(:cache!) }
   it { is_expected.to respond_to(:cached?) }
 
   let(:url) { "http://blog.twingly.com/#{SecureRandom.hex}" }
+
+  describe "#ttl" do
+    subject { cache.ttl }
+
+    context "with default ttl (no expire)" do
+      it { is_expected.to be(0) }
+    end
+
+    context "when set to custom value" do
+      let(:ttl) { 3 }
+      subject { described_class.new(ttl: ttl).ttl }
+      it { is_expected.to be(ttl) }
+    end
+  end
 
   describe "#cache!" do
     subject { cache.cache!(url) }
@@ -26,6 +41,20 @@ describe Twingly::UrlCache do
       subject { cache.cached?(url) }
 
       it { is_expected.to be(true) }
+    end
+
+    context "when expired" do
+      let(:ttl) { 1 }
+      let(:cache) { described_class.new(ttl: ttl) }
+
+      before do
+        cache.cache!(url)
+        sleep ttl * 2
+      end
+
+      subject { cache.cached?(url) }
+
+      it { is_expected.to be(false) }
     end
   end
 end
